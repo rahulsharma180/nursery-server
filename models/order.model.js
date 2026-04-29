37600858d07eb31a003bd3e51cd0994b4c5a974e
Digitalamitchoudhary <amitchoudhary.dmp@gmail.com>
2026-04-30 00:20:33 +0530

    fs

--- models/order.model.js
+++ models/order.model.js
@@ -1,11 +1,17 @@
 import mongoose from "mongoose";
 
 const orderSchema = new mongoose.Schema({
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    require: true,
+  },
+  orderId:{
+    type:String,
+    unique:true,
+    require:true
+  }
+  
+});
 
-
-
-})
-
-
-const OrderModel = mongoose.model("order",orderSchema)
+const OrderModel = mongoose.model("order", orderSchema);
 export default OrderModel;
