9ff27770c16be6c5928b2c941f80ed7d4f279edb
digitalamitchoudhary <amitchoudhary.dmp@gmail.com>
2026-04-30 17:54:03 +0530

    ordr

--- models/order.model.js
+++ models/order.model.js
@@ -5,12 +5,45 @@ const orderSchema = new mongoose.Schema({
     type: mongoose.Schema.Types.ObjectId,
     require: true,
   },
-  orderId:{
-    type:String,
-    unique:true,
-    require:true
+  orderId: {
+    type: String,
+    unique: true,
+    require: true
+  },
+  items: [{
+    product: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "product",
+      require: true
+    },
+    name: {
+      type: string,
+      require: true
+    },
+    quantity: {
+      type: Number,
+    },
+    price: {
+      type: Number,
+      require: true
+    },
+    size: {
+       type: String
+      },
+      
+
+
+
+
+
+
+
+
+
+
   }
-  
+  ]
+
 });
 
 const OrderModel = mongoose.model("order", orderSchema);
